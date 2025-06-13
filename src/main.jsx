6f65da7948e4c10964c7b896a64decd9af8ffbdc
Ahmed Khan <ahmedkhan81x@gmail.com>
2025-06-13 15:17:26 +0500

    added layout

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router'
+import { ContextProvider } from './context/Context.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+    <BrowserRouter>
+    <ContextProvider>
     <App />
-  </StrictMode>,
+    </ContextProvider>
+    </BrowserRouter>
+    
 )
